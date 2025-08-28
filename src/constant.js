3ecbbf042424ccb69a94596d7d5898c96c122618
khadimflash <gningkhadim23@gmail.com>
2025-08-28 14:00:41 +0000

    feat: Protect API key using Vite environment variables
    - Update src/constant.js to use import.meta.env.VITE_API_KEY.
    - Create .env file to store the API key.
    - Add .env to .gitignore to prevent it from being committed.
    - Remove broullon.txt.

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const API_KEY = "731a10e628628474e1694ff5304eeb46";
+export const API_KEY = import.meta.env.VITE_API_KEY;
 export const API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
 export const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
